9cd1b378ad4b2ea861597590be443c1fc9c508d5
Omine100 <54552984+Omine100@users.noreply.github.com>
2021-08-08 20:23:09 -0500

    Daily - Firebase update
    Got the firebaseAccounts and firebaseCRUD files setup for a start
    
    The CRUD file only has the create, read, update, and delete methods for the images
    
    The Accounts file has everything related to the account itself
    
    I thought that this might be a good way to split everything up going forward
    
    I still want to look into whether or not there would be a better way to get the currentUser.uid instead of calling it every single time, but it is just a property call I think

---
+++ daily_1.0/daily/lib/servicesBroad/firebaseAccounts.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import 'dart:async';
+
+//METHOD DECLARATIONS
+abstract class BaseCloud {
+  //METHODS: Account management
+  Future<String> getCurrentUserId();
+  Future<bool> getSignedInStatus();
+  Future<void> setCurrentUserDisplayName(String displayName);
+  Future<String> getCurrentUserDisplayName();
+  Future<void> sendEmailVerification();
+  Future<bool> getEmailVerified();
+  Future<void> sendPasswordReset(String email);
+  Future<void> signUpEmailAndPassword(
+      BuildContext context, String email, String password, String name);
+  Future<void> signInEmailAndPassword(
+      BuildContext context, String user_email, String user_password);
+  Future<void> signInGoogle();
+  Future<void> signInTwitter();
+  Future<void> signOut();
+  Future<void> deleteUserData();
+  Future<void> deleteUser();
+}
+
+class FirestoreAccounts implements BaseCloud {
+  //VARIABLE INITIALIZATION
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  //MECHANICS
+  Future<String> getCurrentUserId() async {
+    return auth.currentUser.uid;
+  }
+  
+  Future<bool> getSignedInStatus() async {
+    if (auth.currentUser?.uid == null) 
+      return false;
+    return true;
+  }
+
+  Future<void> setCurrentUserDisplayName(String displayName) async {
+    auth.currentUser.updateDisplayName(displayName);
+  }
+
+  Future<String> getCurrentUserDisplayName() async {
+    return auth.currentUser.displayName;
+  }
+
+  Future<void> sendEmailVerification() async {
+    auth.currentUser.sendEmailVerification();
+  }
+
+  Future<bool> getEmailVerified() async {
+    return auth.currentUser.emailVerified;
+  }
+
+  Future<void> sendPasswordReset(String email) async {
+    auth.sendPasswordResetEmail(email: email);
+  }
+
+  Future<void> signUpEmailAndPassword(BuildContext context, String email, String password, String name) async {
+    try {
+      await auth.createUserWithEmailAndPassword(email: email, password: password);
+      await setCurrentUserDisplayName(name);
+      sendEmailVerification();
+    } on FirebaseAuthException catch (e) {
+      String key;
+      switch (e.code) {
+        case "invalid-password":
+          key = "errorInvalidPassword";
+          break;
+        case "invalid-email":
+          key = "errorInvalidEmail";
+          break;
+        case "email-already-exists":
+          key = "errorEmailAlreadyExists";
+          break;
+        case "invalid-credential":
+          key = "errorInvalidEmail";
+          break;
+        default:
+          key = "errorDefault";
+      }
+      InterfaceStandards().showToastMessage(context, key);
+    }
+  }
+
+  Future<void> signInEmailAndPassword(BuildContext context, String user_email, String user_password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: user_email, password: user_password);
+    } on FirebaseAuthException catch (e) {
+      String key;
+      switch (e.code) {
+        case "invalid-email":
+          key = "errorInvalidEmail";
+          break;
+        case "wrong-password":
+          key = "errorWrongPassword";
+          break;
+        case "user-not-found":
+          key = "errorUserNotFound";
+          break;
+        case "user-disabled":
+          key = "errorUserDisabled";
+          break;
+        default:
+          key = "errorDefault";
+      }
+      InterfaceStandards().showToastMessage(context, key);
+    }
+  }
+
+  Future<bool> signInGoogle() async {
+    final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication result = await googleUser.authentication;
+    final GoogleAuthCredential googleCredential = GoogleAuthProvider.credential(
+        accessToken: result.accessToken, idToken: result.idToken);
+    final UserCredential userCredential =
+        await auth.signInWithCredential(googleCredential);
+    if (userCredential.additionalUserInfo.isNewUser)
+      await setCurrentUserDisplayName(googleUser.displayName);
+    return userCredential.additionalUserInfo.isNewUser;
+  }
+
+  //BEARER TOKEN: AAAAAAAAAAAAAAAAAAAAAAktOwEAAAAAnUDLMWtzIyxZLpHanZZokQruzOc%3
+  //           DKBrBIFVdiw9OvaN5vcAAU4v8jCgO6dOluSp5K7smTXM9RBgMWj
+  Future<bool> signInTwitter() async { //Need to change keys
+    final TwitterLogin twitterLogin = new TwitterLogin(
+      consumerKey: '06tLwhwnkvJeK5smvt9gPOTe5',
+      consumerSecret: 'ob0s5XtYYJTEUX4GptaruP1n5Zvvq2hGyyHDeYkPylDD4RdOAC',
+    );
+    final TwitterLoginResult result = await twitterLogin.authorize();
+    if (result.status == TwitterLoginStatus.loggedIn) {
+      final AuthCredential twitterCredential = TwitterAuthProvider.credential(
+          accessToken: result.session.token, secret: result.session.secret);
+      final UserCredential userCredential =
+          await auth.signInWithCredential(twitterCredential);
+      if (userCredential.additionalUserInfo.isNewUser)
+        await setCurrentUserDisplayName(result.session.username);
+      return userCredential.additionalUserInfo.isNewUser;
+    } else if (result.status == TwitterLoginStatus.cancelledByUser)
+      return null;
+    else
+      return null;
+  }
+
+  Future<void> signOut() async {
+    return auth.signOut();
+  }
+
+  Future<void> deleteUserData() async {
+    var userId = auth.currentUser.uid;
+    await firestore.collection(userId).get().then((snapshot) {
+      for (DocumentSnapshot ds in snapshot.docs) {
+        ds.reference.delete();
+      }
+    });
+  }
+  
+  Future<void> deleteUser() async {
+    deleteUserData();
+    auth.currentUser.delete();
+  }
+}
---
+++ daily_1.0/daily/lib/servicesBroad/firebaseCRUD.dart
@@ -0,0 +1,88 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:async';
+import 'dart:io';
+
+//METHOD DECLARATIONS
+abstract class BaseCloud {
+  //METHODS: Data management
+  Future<void> createImageData(BuildContext context, String fileName, File imageFile);
+  Future<List<DocumentSnapshot>> getImageDocuments();
+  Future<void> updateImageData(BuildContext context, String imageURL, File imageFile);
+  Future<void> deleteImageData(DocumentSnapshot doc, String imageURL);
+}
+
+class FirebaseCRUD implements BaseCloud {
+  //CLASS INITIALIZATION
+  MethodStandards methodStandards = new MethodStandards();
+
+  //VARIABLE INITIALIZATION
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  FirebaseStorage storage = FirebaseStorage.instance;
+
+  //MECHANICS
+  Future<void> createImageData(BuildContext context, String fileName, File imageFile) async {
+    var userId = auth.currentUser.uid;
+    String date = methodStandards.getCurrentDate();
+    try {
+      await storage
+          .ref(fileName)
+          .putFile(imageFile, SettableMetadata(customMetadata: {'date': date}));
+      storage
+          .ref(fileName)
+          .getDownloadURL()
+          .then((_imageURL) => {
+            firestore
+              .collection(userId)
+              .doc("images")
+              .collection("complete")
+              .doc(date)
+              .set({"imageURL": _imageURL})
+          });
+    } on FirebaseException catch (e) {
+      InterfaceStandards().showToastMessage(context, "errorImageFailedToUpload");
+    }
+  }
+
+  Future<List<DocumentSnapshot>> getImageDocuments() async {
+    var userId = auth.currentUser.uid;
+    final QuerySnapshot querySnapshot = await firestore
+        .collection(userId)
+        .doc("images")
+        .collection("complete")
+        .get();
+    final List<DocumentSnapshot> documentSnapshots = querySnapshot.docs;
+    List<DocumentSnapshot> documentSnapshotsReversed = [];
+    for (int i = documentSnapshots.length - 1; i >= 0; i--) {
+      documentSnapshotsReversed.add(documentSnapshots.elementAt(i));
+    }
+    return documentSnapshotsReversed;
+  }
+  
+  Future<void> updateImageData(BuildContext context, String imageURL, File imageFile) async {
+    try {
+      await storage.refFromURL(imageURL).putFile(imageFile);
+    } on FirebaseException catch (e) {
+      InterfaceStandards().showToastMessage(context, "errorImageFailedToUpload");
+    }
+  }
+  
+  Future<void> deleteImageData(DocumentSnapshot doc, String imageURL) async {
+    //MECHANICS: Firebase deletion
+    Reference reference = FirebaseStorage.instance.refFromURL(imageURL);
+    reference.delete();
+
+    //MECHANICS: Firestore deletion
+    var userID = auth.currentUser.uid;
+    await firestore
+        .collection(userID)
+        .doc("images")
+        .collection("complete")
+        .doc(doc.id)
+        .delete();
+  }
+}
