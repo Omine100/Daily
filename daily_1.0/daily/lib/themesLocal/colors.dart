d51cb129a19c8e4594cba20561e59e5eb2c579d6
Omine100 <54552984+Omine100@users.noreply.github.com>
2021-09-02 18:26:32 -0500

    Update colors.dart

--- daily_1.0/daily/lib/themesLocal/colors.dart
+++ daily_1.0/daily/lib/themesLocal/colors.dart
@@ -8,76 +8,28 @@ extension CustomColorScheme on ColorScheme {
   // #endregion
 
   // #region gradients
-  Color gradientsBackgroundTopRight() {
-    return IsDark() ? Color(0xFF102449) : Color(0xFFFF99D1);
-  }
-
-  Color gradientsBackgroundBottomLeft() {
-    return IsDark() ? Color(0xFF1B1B2D) : Color(0xFFFFAB5F);
-  }
+  Color get gradientsBackgroundTopRight => IsDark() ? Color(0xFF102449) : Color(0xFFFF99D1);
+  Color get gradientsBackgroundBottomLeft => IsDark() ? Color(0xFF1B1B2D) : Color(0xFFFFAB5F);
   // #endregion
 
   // #region userIStandards
-  Color userIStandardsCameraButton() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsThemeSelector() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsLanguageSelectorBackground() {
-    return IsDark() ? Color(0xFF102457) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsLanguageSelectorContent() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
-  }
-
-  Color userIStandardsLanguageSelectorIcon() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
-  }
-
-  Color userIStandardsShareButton() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFFBDBDBD);
-  }
-
-  Color userIStandardsSocialButton() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsToastMessageContent() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFFBDBDBD);
-  }
-
-  Color userIStandardsToastMessageBackground() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsBackButton() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
-  }
-
-  Color userIStandardsTitleContent() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsTextInputContent() {
-    return IsDark() ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsTextInputLine() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
-
-  Color userIStandardsTextInputIcon() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
+  Color get userIStandardsCameraButton => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
+  Color get userIStandardsThemeSelector => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
+  Color get userIStandardsLanguageSelectorBackground => IsDark() ? Color(0xFF102457) : Color(0xFFFFFFFF);
+  Color get userIStandardsLanguageSelectorContent => IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
+  Color get userIStandardsLanguageSelectorIcon => IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
+  Color get userIStandardsShareButton => IsDark() ? Color(0xFFFFFFFF) : Color(0xFFBDBDBD);
+  Color get userIStandardsSocialButton => IsDark() ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get userIStandardsToastMessageContent() => IsDark() ? Color(0xFFFFFFFF) : Color(0xFFBDBDBD);
+  Color get userIStandardsToastMessageBackground => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
+  Color get userIStandardsBackButton => IsDark() ? Color(0xFFFFFFFF) : Color(0xFF000000);
+  Color get userIStandardsTitleContent => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
+  Color get userIStandardsTextInputContent => IsDark() ? Color(0xFFFFFFFF) : Color(0xFFFFFFFF);
+  Color get userIStandardsTextInputLine => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
+  Color get userIStandardsTextInputIcon => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
   // #endregion
 
   // #region userXStandards
-  Color userXStandardsProgressIndicator() {
-    return IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
-  }
+  Color get userXStandardsProgressIndicator => IsDark() ? Color(0xFFFFAB58) : Color(0xFFFFFFFF);
   // #endregion
 }
