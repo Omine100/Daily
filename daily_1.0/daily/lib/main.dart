1d97a8268c007bdfe346ef80f8eacac98ae7f766
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-09-09 21:26:54 -0500

    Update main.dart
    Working on main.dart

--- daily_1.0/daily/lib/main.dart
+++ daily_1.0/daily/lib/main.dart
@@ -1,1 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => {runApp(new Daily())});
+}
+
+class Daily extends StatefulWidget {
+  static void setLocale(BuildContext context, Locale locale) {
+    _DailyState state = context.findAncestorStateOfType<_DailyState>();
+    state.setLocale(locale);
+  }
+
+  @override
+  _DailyState createState() => _DailyState();
+}
+
+class _DailyState extends State<Daily> {
+  @override
+  Widget build(BuildContext context) {}
+}
