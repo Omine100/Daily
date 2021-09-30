d71b10a3cae0ab62a40f1b4438b7b3bc7840aae9
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-09-29 20:53:53 -0500

    neumorphic update

--- daily_1.0/daily/lib/servicesLocal/colorManagement.dart
+++ daily_1.0/daily/lib/servicesLocal/colorManagement.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-Color adjust(Color color, double amount) {
+Color colorShade(Color color, double amount) {
   assert(amount >= -1 && amount <= 1);
 
   final hsl = HSLColor.fromColor(color);
@@ -9,4 +10,18 @@ Color adjust(Color color, double amount) {
         .withLightness((hsl.lightness - amount).clamp(0.0, 1.0))
         .toColor();
   return hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0)).toColor();
+}
+
+Color colorShadow(Color base, double intensity) {
+  final t = intensity ?? 0;
+  String e = base.value.toRadixString(16).substring(2);
+  if (e.length < 6) e = e[0] + e[0] + e[1] + e[1] + e[2] + e[2];
+  String o = '';
+  for (int n = 0; n < 3; n++) {
+    var a = int.parse(e.substring(2 * n, 2 * n + 2), radix: 16);
+    a = min(max(0, (a + a * t).round()), 255);
+    o += ('00' + a.toRadixString(16)).substring(a.toRadixString(16).length);
+  }
+
+  return Color(int.parse(o, radix: 16)).withAlpha(255);
 }
--- daily_1.0/daily/lib/standards/neumorphic/neumorphic.dart
+++ daily_1.0/daily/lib/standards/neumorphic/neumorphic.dart
@@ -2,15 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:daily/servicesLocal/colorManagement.dart';
 import 'package:daily/servicesLocal/systemPreferences.dart';
 
+enum NeumorphicLightSource {
+  topLeft,
+  topRight,
+  bottomLeft,
+  bottomRight,
+}
+
+enum NeumorphicShape {
+  concave,
+  convex,
+  emboxx,
+  flat,
+}
+
+Offset sourceToOffset(NeumorphicLightSource lightSource, double distance) {
+  Offset offset;
+  switch (lightSource) {
+    case NeumorphicLightSource.topLeft:
+      offset = Offset(distance, distance);
+      break;
+    case NeumorphicLightSource.topRight:
+      offset = Offset(-distance, distance);
+      break;
+    case NeumorphicLightSource.bottomLeft:
+      offset = Offset(distance, -distance);
+      break;
+    case NeumorphicLightSource.bottomRight:
+      offset = Offset(-distance, -distance);
+      break;
+  }
+  return offset;
+}
+
 extension Neumorphic on Widget {
   addWrapper({
-    double depth = 0.0,
+    Duration duration = const Duration(milliseconds: 100),
     double borderRadius = 0.0,
-    double blurRadius = 0.0,
     double height = 0.0,
     double width = 0.0,
-    Duration duration = const Duration(milliseconds: 100),
+    double distance = 0.0,
+    NeumorphicLightSource lightSource = NeumorphicLightSource.topLeft,
+    double blurRadius = 0.0,
     Offset blurOffset = const Offset(0, 0),
+    NeumorphicShape shape = NeumorphicShape.convex,
     Color backgroundColor = const Color(0x00FFFFFF),
     bool isEnabled = false,
   }) {
@@ -25,12 +60,12 @@ extension Neumorphic on Widget {
           BoxShadow(
             offset: blurOffset,
             blurRadius: blurRadius,
-            color: adjust(backgroundColor, isDark ? 0.2 : -0.2),
+            color: colorShade(backgroundColor, isDark ? 0.2 : -0.2),
           ),
           BoxShadow(
             offset: Offset(-blurOffset.dx, -blurOffset.dx),
             blurRadius: blurRadius,
-            color: adjust(backgroundColor, isDark ? -0.2 : 0.2),
+            color: colorShade(backgroundColor, isDark ? -0.2 : 0.2),
           ),
         ],
       ),
