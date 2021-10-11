0630c04eaef85ccb75827d7285b5b7dff91344e4
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-10-11 12:01:52 -0400

    loginComponents.dart update

--- daily_1.0/daily/lib/userInterface/loginComponents.dart
+++ daily_1.0/daily/lib/userInterface/loginComponents.dart
@@ -8,13 +8,12 @@ class LoginUserInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-    children: [
-      GestureDetector(
-        onTap: () {
-          RouteNavigation().routePage(context, HomeScreen());
-        },
-        child: Column(
-          children: [
+      children: [
+        GestureDetector(
+          onTap: () {
+            RouteNavigation().routePage(context, HomeScreen());
+          },
+          child: Column(children: [
             SizedBox(
               height: 175,
             ),
@@ -23,14 +22,14 @@ class LoginUserInput extends StatelessWidget {
               "Hello",
               style: TextStyle(color: Colors.white, fontSize: 20),
             )).addWrapperGlass(
-                borderRadius: 16,
-                height: 100,
-                width: 400,
-                shadowBlur: 24,
-                containerSpread: 16,
-                backdropBlur: 10,
-                colorOpacity: 0.13,
-              ),
+              borderRadius: 16,
+              height: 100,
+              width: 400,
+              shadowBlur: 24,
+              containerSpread: 16,
+              backdropBlur: 10,
+              colorOpacity: 0.13,
+            ),
             SizedBox(
               height: 50,
             ),
@@ -39,13 +38,13 @@ class LoginUserInput extends StatelessWidget {
               "Testing",
               style: TextStyle(color: Colors.white, fontSize: 20),
             )).addWrapperGlass(
-                borderRadius: 16,
-                height: 300,
-                width: 400,
-                shadowBlur: 24,
-                containerSpread: 16,
-                backdropBlur: 10,
-                colorOpacity: 0.13,
+              borderRadius: 16,
+              height: 300,
+              width: 400,
+              shadowBlur: 24,
+              containerSpread: 16,
+              backdropBlur: 10,
+              colorOpacity: 0.13,
             ),
             SizedBox(
               height: 75,
@@ -53,15 +52,14 @@ class LoginUserInput extends StatelessWidget {
             Padding(
               padding: EdgeInsets.only(left: 400, right: 400),
               child: Divider(
-                  color: Colors.white,
-                  thickness: 1.5,
-                ),
+                color: Colors.white,
+                thickness: 1.5,
+              ),
             ),
-          ]
+          ]),
         ),
-      ),
-    ],
-  );
+      ],
+    );
   }
 }
 
@@ -69,28 +67,46 @@ class LoginSocialIcons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: FaIcon(FontAwesomeIcons.google, size: 40, color: Colors.white,), 
-                  onPressed: () {}
-                ),
-                SizedBox(width: 10,),
-                IconButton(
-                  icon: FaIcon(FontAwesomeIcons.linkedin, size: 40, color: Colors.white,), 
-                  onPressed: () {}
-                ),
-                SizedBox(width: 10,),
-                IconButton(
-                  icon: FaIcon(FontAwesomeIcons.twitter, size: 40, color: Colors.white,), 
-                  onPressed: () {}
-                ),
-                SizedBox(width: 10,),
-                IconButton(
-                  icon: FaIcon(FontAwesomeIcons.facebook, size: 40, color: Colors.white,), 
-                  onPressed: () {}
-                ),
-              ],
-            );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.google,
+              size: 40,
+              color: Colors.white,
+            ),
+            onPressed: () {}),
+        SizedBox(
+          width: 10,
+        ),
+        IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.linkedin,
+              size: 40,
+              color: Colors.white,
+            ),
+            onPressed: () {}),
+        SizedBox(
+          width: 10,
+        ),
+        IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.twitter,
+              size: 40,
+              color: Colors.white,
+            ),
+            onPressed: () {}),
+        SizedBox(
+          width: 10,
+        ),
+        IconButton(
+            icon: FaIcon(
+              FontAwesomeIcons.facebook,
+              size: 40,
+              color: Colors.white,
+            ),
+            onPressed: () {}),
+      ],
+    );
   }
 }
