1cd66bee1a45d6fa61a96b6f437677a3c4a1fbac
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-10-14 15:28:30 -0400

    Update imageManagement.dart

--- daily_1.0/daily/lib/utilities/managementUtil/imageManagement.dart
+++ daily_1.0/daily/lib/utilities/managementUtil/imageManagement.dart
@@ -6,22 +6,20 @@ Color averageImageColor(String imagePath) {
   img.Image bitmap =
       img.decodeImage(new File(imagePath).readAsBytesSync());
 
-  int redBucket = 0;
-  int greenBucket = 0;
-  int blueBucket = 0;
-  int pixelCount = 0;
+  //[pixelCount, red, green, blue]
+  var pixelData = [0, 0, 0, 0];
 
   for (int y = 0; y < bitmap.height; y++) {
     for (int x = 0; x < bitmap.width; x++) {
       int c = bitmap.getPixel(x, y);
 
-      pixelCount++;
-      redBucket += img.getRed(c);
-      greenBucket += img.getGreen(c);
-      blueBucket += img.getBlue(c);
+      pixelData[0]++;
+      pixelData[1] += img.getRed(c);
+      pixelData[2] += img.getGreen(c);
+      pixelData[3] += img.getBlue(c);
     }
   }
 
-  Color averageColor = Color.fromRGBO(redBucket ~/ pixelCount,
-      greenBucket ~/ pixelCount, blueBucket ~/ pixelCount, 1);
+  return Color.fromRGBO(pixelData[1] ~/ pixelData[0],
+      pixelData[2] ~/ pixelData[0], pixelData[3] ~/ pixelData[0], 1);
 }
