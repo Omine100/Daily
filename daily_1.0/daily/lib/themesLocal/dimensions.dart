1ce00c2c269822591d795543ee48c303129000fd
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-10-27 14:08:34 -0400

    Update dimensions.dart

--- daily_1.0/daily/lib/themesLocal/dimensions.dart
+++ daily_1.0/daily/lib/themesLocal/dimensions.dart
@@ -7,6 +7,24 @@ extension CustomDimensions on VisualDensity {
   double get userIStandardsSocialButtonHeight => 0.05;
   double get userIStandardsSocialButtonWidth => 0.05;
   // #endregion
+
+  // #region welcomeDesktop
+  double get welcomeDesktopTitleWidth => 1.0;
+  double get welcomeDesktopUserInputTop => 0.75;
+  double get welcomeDesktopAccountAlreadyTop => 0.925;
+  // #endregion
+
+  // #region welcomeTablet
+  double get welcomeTabletTitleWidth => 1.0;
+  double get welcomeTabletUserInputTop => 0.75;
+  double get welcomeTabletAccountAlreadyTop => 0.925;
+  // #endregion
+
+  // #region welcomeMobile
+  double get welcomeMobileTitleWidth => 1.0;
+  double get welcomeMobileUserInputTop => 0.75;
+  double get welcomeMobileAccountAlreadyTop => 0.925;
+  // #endregion
 }
 
 double getDimension(BuildContext context, bool isHeight, double value) {
