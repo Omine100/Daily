c84d7a9d7f1aae41aa60548da1b1116ce88cfbcf
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-12-23 17:29:47 -0600

    settings update

--- daily_1.0/daily/lib/datastructures/settingState.dart
+++ daily_1.0/daily/lib/datastructures/settingState.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 
 enum Format { DropDown, List, Switch, DateSelector, URL }
 
+enum Group { User, General, Privacy, Hidden }
+
 class Setting {
   String key;
   dynamic value;
   dynamic type;
+  Group group;
   Format format;
   dynamic defaultValue;
 
@@ -13,6 +16,7 @@ class Setting {
       {@required this.key,
       @required this.value,
       @required this.type,
+      @required this.group,
       @required this.format,
       @required this.defaultValue});
 }
--- daily_1.0/daily/lib/servicesLocal/settings.dart
+++ daily_1.0/daily/lib/servicesLocal/settings.dart
@@ -5,30 +5,35 @@ Setting locale = new Setting(
     key: "locale",
     value: "en",
     type: Locale,
+    group: Group.General,
     format: Format.DropDown,
     defaultValue: "en");
 Setting languageCode = new Setting(
     key: "languageCode",
     value: "en",
     type: String,
+    group: Group.General,
     format: Format.List,
     defaultValue: "en");
 Setting isDark = new Setting(
     key: "isDark",
     value: false,
     type: bool,
+    group: Group.General,
     format: Format.Switch,
     defaultValue: false);
 Setting isAndroid = new Setting(
     key: "isAndroid",
     value: true,
     type: bool,
+    group: Group.Hidden,
     format: Format.Switch,
     defaultValue: true);
 Setting profileURL = new Setting(
     key: "profileURL",
     value: "",
     type: String,
+    group: Group.User,
     format: Format.URL,
     defaultValue: "");
 
