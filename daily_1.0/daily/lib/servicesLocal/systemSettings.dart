9be2d09595f6cae938aa8a967f6d04bfb3678a1a
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2021-12-23 11:38:08 -0600

    Update systemSettings.dart

--- daily_1.0/daily/lib/servicesLocal/systemSettings.dart
+++ daily_1.0/daily/lib/servicesLocal/systemSettings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:daily/datastructures/settingState.dart';
 
 Setting locale = new Setting(
@@ -16,6 +17,70 @@ List<Setting> settingsList() {
   return [locale, languageCode, isDark, isAndroid, profileURL];
 }
 
-void saveToPrefs(List<Setting> settingsList) {
-  settingsList.forEach((setting) {});
+SharedPreferences prefs;
+
+prefsInstance() async {
+  prefs ??= await SharedPreferences.getInstance();
+}
+
+void prefsToSettings() async {
+  await prefsInstance();
+  if (prefs.getKeys() == null) {
+    initializePrefs();
+  }
+  prefs.getKeys().forEach((key) {
+    switch (prefs.get(key)) {
+      case "locale":
+        locale.value = prefs.get(key);
+        break;
+      case "languageCode":
+        languageCode.value = prefs.get(key);
+        break;
+      case "isDark":
+        isDark.value = prefs.get(key);
+        break;
+      case "isAndroid":
+        isAndroid.value = prefs.get(key);
+        break;
+      case "profileURL":
+        profileURL.value = prefs.get(key);
+        break;
+      default:
+        return;
+    }
+  });
+}
+
+void settingsToPrefs(List<Setting> settingsList) {
+  settingsList.forEach((setting) {
+    saveToPrefs(setting.key, setting.value);
+  });
+}
+
+saveToPrefs(String key, dynamic value) async {
+  await prefsInstance();
+  switch (value.runtimeType) {
+    case int:
+      prefs.setInt(key, value);
+      break;
+    case double:
+      prefs.setDouble(key, value);
+      break;
+    case bool:
+      prefs.setBool(key, value);
+      break;
+    case String:
+      prefs.setString(key, value);
+      break;
+    default:
+      prefs.setString(key, value.toString());
+  }
+}
+
+initializePrefs() {
+  saveToPrefs("locale", "en");
+  saveToPrefs("languageCode", "en");
+  saveToPrefs("isDark", false);
+  saveToPrefs("isAndroid", true);
+  saveToPrefs("profileURL", "");
 }
