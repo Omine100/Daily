847a22abcff3f27f754aa213f8ac6374b7ae3a46
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2022-01-18 10:25:33 -0500

    userStandards update

--- daily_1.0/daily/lib/standards/userXStandards.dart
+++ daily_1.0/daily/lib/standards/userXStandards.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:daily/themesLocal/colors.dart';
 
-class UserXStandards {
-  Widget showProgress(BuildContext context) {
-    return CircularProgressIndicator(
-      backgroundColor:
-          Theme.of(context).colorScheme.userXStandardsProgressIndicator,
-    );
-  }
+Widget showProgress(BuildContext context) {
+  return CircularProgressIndicator(
+    backgroundColor:
+        Theme.of(context).colorScheme.userXStandardsProgressIndicator,
+  );
 }
