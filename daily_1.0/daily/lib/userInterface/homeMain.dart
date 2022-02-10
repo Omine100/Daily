d0172c3e05f819b99b9fd975a2aa44735e8a754a
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2022-02-10 09:02:34 -0500

    homeMain updates
    Think Im going to go with a camera on the first page for the time being just so that I can get something down. Plus I would love it if I didn't have to make the user open up a camera
    
    Want to look into make the home scrollable

--- daily_1.0/daily/lib/userInterface/homeMain.dart
+++ daily_1.0/daily/lib/userInterface/homeMain.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 import 'package:daily/servicesLocal/responsive.dart';
 import 'package:daily/userInterface/homeMainComponents.dart';
 
