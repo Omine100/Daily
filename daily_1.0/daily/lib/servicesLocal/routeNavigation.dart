e220109dfb0afa661b36e43f2f462a3a955bf55a
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2022-02-23 14:21:43 -0500

    Update routeNavigation.dart
    Need to get routing setup for 2.0 w/ autoRoute

--- daily_1.0/daily/lib/servicesLocal/routeNavigation.dart
+++ daily_1.0/daily/lib/servicesLocal/routeNavigation.dart
@@ -1,25 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:daily/userInterface/welcome.dart';
-import 'package:daily/userInterface/auth.dart';
-import 'package:daily/userInterface/home.dart';
 import 'package:daily/userInterface/textViewer.dart';
 import 'package:daily/userInterface/imageViewer.dart';
 
 class RouteNavigation {
-  void routePop(BuildContext context) {
-    Navigator.pop(context);
-  }
-
-  void routeBase(BuildContext context, Widget page) {
-    Navigator.pushAndRemoveUntil(context,
-        MaterialPageRoute(builder: (context) => page), (route) => false);
-  }
-
-  void routePage(BuildContext context, Widget page) {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
-  }
-
   void routeTextViewer(BuildContext context, String filePath) {
     Navigator.push(
         context,
@@ -35,15 +18,5 @@ class RouteNavigation {
                   image: image,
                   imagePath: imagePath,
                 )));
-  }
-
-  Widget routeInitial(BuildContext context, bool isSignedIn) {
-    return isSignedIn
-        ? HomeScreen()
-        : (kIsWeb
-            ? AuthScreen(
-                isSignIn: true,
-              )
-            : WelcomeScreen());
   }
 }
