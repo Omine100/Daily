79c1d434bca0016cdcf6c61165f0fafbdb7b96b8
Matthew Browning <54552984+Omine100@users.noreply.github.com>
2022-03-09 13:23:16 -0500

    ForgotPassword UI updates
    Getting there on forgotPassword, I think that I can probably finish that tomorrow? We'll see.
    
    After that, I need to do ImageViewer updates, and then that should pretty much just leave the home pages

--- daily_1.0/daily/lib/userInterface/forgotPassword/forgotPasswordWebComponents.dart
+++ daily_1.0/daily/lib/userInterface/forgotPassword/forgotPasswordWebComponents.dart
@@ -6,6 +6,8 @@ import 'package:daily/themesLocal/dimensions.dart';
 import 'package:daily/themesLocal/fontSizes.dart';
 import 'package:daily/themesLocal/fontWeights.dart';
 
+FirebaseAccounts firebaseAccounts = new FirebaseAccounts();
+
 Widget forgotPasswordWebCard(BuildContext context) {
   return Container(
     height: getDimension(context, true,
@@ -65,4 +67,171 @@ Widget forgotPasswordWebTitle(BuildContext context) {
       ],
     ),
   );
+}
+
+Widget forgotPasswordCenterPiece(BuildContext context) {
+  return Container(
+      height: getDimension(context, true,
+          Theme.of(context).visualDensity.forgotPasswordCenterPieceHeight),
+      width: getDimension(context, false,
+          Theme.of(context).visualDensity.forgotPasswordCenterPieceWidth),
+      child: Image(
+          image: AssetImage(
+              "lib/assets/forgotPassword/mobile/forgotPassword_centerPiece.png")));
+}
+
+String userEmail;
+final GlobalKey<FormFieldState> formKey = GlobalKey<FormFieldState>();
+Widget forgotPasswordUserInputField(BuildContext context) {
+  return Container(
+    height: getDimension(context, true,
+        Theme.of(context).visualDensity.forgotPasswordUserInputFieldHeight),
+    width: getDimension(context, false,
+        Theme.of(context).visualDensity.forgotPasswordUserInputFieldWidth),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(30),
+      color: Theme.of(context).colorScheme.forgotPasswordMobileUserInputField,
+    ),
+    child: TextFormField(
+      key: formKey,
+      obscureText: false,
+      onSaved: (email) => userEmail = email,
+      autofocus: false,
+      style: TextStyle(
+          color: Theme.of(context)
+              .colorScheme
+              .forgotPasswordMobileUserInputFieldText),
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        hintText: getTranslated(context, "forgotPasswordFormEmail"),
+        labelStyle: TextStyle(
+          color: Theme.of(context)
+              .colorScheme
+              .forgotPasswordMobileUserInputFieldDecoration,
+          fontSize: Theme.of(context)
+              .textTheme
+              .forgotPasswordUserInputFieldDecoration,
+          fontWeight: Theme.of(context)
+              .typography
+              .forgotPasswordUserInputFieldDecoration,
+        ),
+        hintStyle: TextStyle(
+          color: Theme.of(context)
+              .colorScheme
+              .forgotPasswordMobileUserInputFieldDecoration,
+          fontSize: Theme.of(context)
+              .textTheme
+              .forgotPasswordUserInputFieldDecoration,
+          fontWeight: Theme.of(context)
+              .typography
+              .forgotPasswordUserInputFieldDecoration,
+        ),
+        prefixIcon: Icon(
+          Icons.email,
+          color: Theme.of(context)
+              .colorScheme
+              .forgotPasswordMobileUserInputFieldIconDecoration,
+        ),
+      ),
+    ),
+  );
+}
+
+bool isSent = false;
+Widget forgotPasswordSend(BuildContext context, State state) {
+  return Center(
+    child: Container(
+      height: getDimension(context, true,
+          Theme.of(context).visualDensity.forgotPasswordSendHeight),
+      width: getDimension(context, false,
+          Theme.of(context).visualDensity.forgotPasswordSendWidth),
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.forgotPasswordMobileSend,
+          borderRadius: BorderRadius.circular(30)),
+      child: Material(
+        color: Theme.of(context).colorScheme.materialTransparent,
+        child: InkWell(
+          splashColor:
+              Theme.of(context).colorScheme.forgotPasswordMobileSendInkWell,
+          customBorder:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          onTap: () {
+            formKey.currentState.save();
+            firebaseAccounts
+                .sendPasswordReset(context, userEmail)
+                .then((_isSent) => {
+                      if (_isSent)
+                        {
+                          state.setState(() {
+                            isSent = true;
+                          })
+                        }
+                    });
+            formKey.currentState.reset();
+          },
+          child: Container(
+            child: Center(
+              child: Text(
+                getTranslated(context, "forgotPasswordSend"),
+                style: TextStyle(
+                  color: Theme.of(context)
+                      .colorScheme
+                      .forgotPasswordMobileSendText,
+                  fontSize: Theme.of(context).textTheme.forgotPasswordSendText,
+                  fontWeight:
+                      Theme.of(context).typography.forgotPasswordSendText,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget forgotPasswordResend(BuildContext context) {
+  return isSent
+      ? GestureDetector(
+          onTap: () {
+            formKey.currentState.save();
+            firebaseAccounts.sendPasswordReset(context, userEmail);
+          },
+          child: RichText(
+            text: TextSpan(
+              text: getTranslated(context, "forgotPasswordResendPrimary"),
+              style: TextStyle(
+                color: Theme.of(context)
+                    .colorScheme
+                    .forgotPasswordMobileResendPrimary,
+                fontSize:
+                    Theme.of(context).textTheme.forgotPasswordResendPrimary,
+                fontWeight:
+                    Theme.of(context).typography.forgotPasswordResendPrimary,
+              ),
+              children: <TextSpan>[
+                TextSpan(
+                  text: getTranslated(context, "forgotPasswordResendSecondary"),
+                  style: TextStyle(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .forgotPasswordMobileResendSecondary,
+                    fontSize: Theme.of(context)
+                        .textTheme
+                        .forgotPasswordResendSecondary,
+                    fontWeight: Theme.of(context)
+                        .typography
+                        .forgotPasswordResendSecondary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      : Container();
+}
+
+void forgotPasswordDispose() {
+  isSent = false;
 }
