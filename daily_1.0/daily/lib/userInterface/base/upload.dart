f0ffeee3090002f7c5fc0e5a7e5829aa48b44b28
Omine100 <54552984+Omine100@users.noreply.github.com>
2022-05-26 21:48:35 -0500

    Upload update

--- daily_1.0/daily/lib/userInterface/base/upload.dart
+++ daily_1.0/daily/lib/userInterface/base/upload.dart
@@ -1,7 +1,9 @@
+import 'package:daily/servicesBroad/firebaseAccounts.dart';
 import 'package:daily/servicesLocal/adaptive.dart';
 import 'package:flutter/material.dart';
 import 'package:daily/servicesLocal/hover.dart';
 import 'package:daily/themesLocal/colors.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Upload extends StatefulWidget {
   @override
@@ -9,19 +11,25 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  FirebaseAccounts _firebaseAccounts = new FirebaseAccounts();
+  OverlayState overlayState;
+  OverlayEntry overlayBackground;
+  OverlayEntry overlayBase;
+  OverlayEntry overlayUploadStep1;
+  OverlayEntry overlayUploadStep2;
   int _step = 0;
 
   void _showOverlay(BuildContext context) async {
-    OverlayState overlayState = Overlay.of(context);
-    OverlayEntry overlayEntry;
-    OverlayEntry overlayBackground;
+    overlayState = Overlay.of(context);
 
     overlayBackground = OverlayEntry(builder: (context) {
       return Center(
         child: GestureDetector(
           onTap: () {
-            overlayEntry.remove();
             overlayBackground.remove();
+            overlayBase.remove();
+            overlayUploadStep1.remove();
+            overlayUploadStep2.remove();
           },
           child: Container(
             height: MediaQuery.of(context).size.height,
@@ -32,7 +40,7 @@ class _UploadState extends State<Upload> {
       );
     });
 
-    overlayEntry = OverlayEntry(builder: (context) {
+    overlayBase = OverlayEntry(builder: (context) {
       return Center(
         child: Container(
           height: MediaQuery.of(context).size.height * 0.7,
@@ -40,13 +48,30 @@ class _UploadState extends State<Upload> {
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: Theme.of(context).colorScheme.baseBackground),
-          child: Center(child: _uploadStep()),
         ),
       );
     });
 
+    overlayUploadStep1 = OverlayEntry(builder: (context) {
+      return Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.7,
+          width: MediaQuery.of(context).size.height * 0.7,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Theme.of(context).colorScheme.baseBackground),
+          child: Center(child: _step == 0 ? step1() : step2()),
+        ),
+      );
+    });
+
+    overlayUploadStep2 = OverlayEntry(builder: (context) {
+      return Container();
+    });
+
     overlayState.insert(overlayBackground);
-    overlayState.insert(overlayEntry);
+    overlayState.insert(overlayBase);
+    overlayState.insert(overlayUploadStep1);
   }
 
   Widget _backButton() {
@@ -62,58 +87,44 @@ class _UploadState extends State<Upload> {
     );
   }
 
-  Widget _forwardButton() {
-    return IconButton(
-      onPressed: () {
-        this.setState(() {
-          _step++;
-        });
-      },
-      icon: Icon(Icons.arrow_forward),
-      iconSize: 25,
-      color: Colors.white,
+  Widget step1() {
+    return Material(
+      color: Theme.of(context).colorScheme.baseBackground,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () async {
+              await getImage();
+              overlayUploadStep1.remove();
+              overlayState.insert(overlayUploadStep2);
+            },
+            icon: Icon(Icons.image),
+            iconSize: 45,
+            color: Colors.white,
+          ),
+          AdaptiveText(
+            "Upload Image",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.w400),
+          )
+        ],
+      ),
     );
   }
 
-  Widget _uploadStep() {
-    switch (_step) {
-      case 0:
-        return Material(
-          color: Theme.of(context).colorScheme.baseBackground,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.image),
-                iconSize: 45,
-                color: Colors.white,
-              ),
-              AdaptiveText(
-                "Upload Image",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400),
-              )
-            ],
-          ),
-        );
-        break;
-      case 1:
-        return Column(
-          children: [],
-        );
-        break;
-      case 2:
-        return Column(
-          children: [],
-        );
-        break;
-      default:
-        return Container();
-    }
+  Widget step2() {
+    return Container();
+  }
+
+  void getImage() async {
+    await ImagePicker()
+        .pickImage(source: ImageSource.gallery)
+        .then((value) async {
+      _firebaseAccounts.setCurrentUserProfilePicData(
+          await value.readAsBytes(), this);
+    });
   }
 
   @override
